97e2dd8b0daad6e8ee429f87fbdcf18e13fd0b06
sandrustout <144401924+sandrustout@users.noreply.github.com>
2024-11-19 13:09:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="wordfilegenerator",
     version="1.0.0",
-    description="Generate wordlist files with custom specifications.",
-    author="sandrustout",
-    url="https://github.com/sandrustout/unlimitedwordfilegenerator",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "generatewordlist=main:main",
-        ],
-    },
+    description="Generate custom wordlists for brute force testing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["your_package_name"],
+    license="MIT",
 )
+
