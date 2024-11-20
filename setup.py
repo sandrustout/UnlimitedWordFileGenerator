3e39efaa28a64707b98499bef9c3273baf6e5bf8
sandrustout <144401924+sandrustout@users.noreply.github.com>
2024-11-20 10:28:27 +0530

    updated version to --3.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="wordfilegenerator",
-    version="1.0.0",
+    version="3.1.2",
     description="Generate custom wordlists for brute force testing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
